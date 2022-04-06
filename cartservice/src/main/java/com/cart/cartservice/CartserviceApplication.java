2980170fe60ef1cd0f906929b6d3a591a4d2fb56
ej <joomn11@gmail.com>
2022-04-06 23:34:27 +0900

    service - add eureka client config

--- cartservice/src/main/java/com/cart/cartservice/CartserviceApplication.java
+++ cartservice/src/main/java/com/cart/cartservice/CartserviceApplication.java
@@ -2,8 +2,10 @@ package com.cart.cartservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CartserviceApplication {
 
 	public static void main(String[] args) {
