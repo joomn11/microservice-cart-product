3dec7502f1eeee0f7ae1e905d61eb263ef2fd423
ej <joomn11@gmail.com>
2022-04-06 19:41:29 +0900

    product-service activemq produce

--- product-service/src/main/java/com/product/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/product/productservice/controller/ProductController.java
@@ -1,11 +1,18 @@
 package com.product.productservice.controller;
 
 import java.util.List;
+import java.util.Optional;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.product.productservice.entity.Product;
 import com.product.productservice.repository.ProductRepository;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +26,12 @@ import lombok.RequiredArgsConstructor;
 public class ProductController {
 
     private final ProductRepository productRepository;
+    private final JmsTemplate jmsTemplate;
+    private final ObjectMapper mapper;
+
+    // get value from yaml file
+    @Value("${product.jms.destination}")
+    private String jmsQueue;
 
     @PostMapping("/addOne")
     public Product addProduct(@RequestBody Product product) {
@@ -33,5 +46,25 @@ public class ProductController {
     @GetMapping("/getAll")
     public List<Product> getAllProducts() {
         return productRepository.findAll();
+    }
+
+    @GetMapping("/sendToCart/{id}")
+    public ResponseEntity<Product> sendToCart(@PathVariable long id) {
+        Optional<Product> product = productRepository.findById(id);
+
+        if (!product.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        try {
+            String jsonInString = mapper.writeValueAsString(product.get());
+
+            jmsTemplate.convertAndSend(jmsQueue, jsonInString);
+
+            return new ResponseEntity<>(product.get(), HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
