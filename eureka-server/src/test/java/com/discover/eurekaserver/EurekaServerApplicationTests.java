86900f21b60436bc9d4cb55e8feffd1cbafab365
ej <joomn11@gmail.com>
2022-04-06 20:05:58 +0900

    add eureka-server

---
+++ eureka-server/src/test/java/com/discover/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.discover.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
